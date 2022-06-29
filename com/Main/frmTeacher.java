ac85a0cc7b92742279c30c3b4f40abee85dd2762
Suhas <108018796+Suhass27@users.noreply.github.com>
2022-06-29 13:13:59 +0530

    Update frmTeacher.java

--- com/Main/frmTeacher.java
+++ com/Main/frmTeacher.java
@@ -18,7 +18,7 @@ import com.Main.frmclassa;
 class Table{  
  synchronized void printTable(int n){//synchronized method  
    for(int i=1;i<=5;i++){  
-     System.out.println(n*i);  
+     System.out.println(Thread.currentThread().getName()+"  "+n*i);  
      try{  
       Thread.sleep(400);  
      }catch(Exception e){System.out.println(e);}  
